1f43201ae73ec6aa9ace6495865ccc6a065c1cfe
BrayanJcr <70727399+BrayanJcr@users.noreply.github.com>
2021-10-02 12:29:16 -0500

    Gantt Funcionando

--- sistemaControlProyectos/Scripts/Views/Gantt.js
+++ sistemaControlProyectos/Scripts/Views/Gantt.js
@@ -1,122 +1,145 @@
-﻿Highcharts.ganttChart('container', {
+﻿
+function cambiarFecha(fecha) {
+    var fechaString = fecha.substr(6);
+    var fechaActual = parseInt(fechaString);
+    return fechaActual;
+}
 
-    title: {
-        text: 'DIAGRAMA DE GANTT'
-    },
-
-    xAxis: {
-        tickPixelInterval: 70
-    },
-
-    yAxis: {
-        type: 'category',
-        grid: {
-            enabled: true,
-            borderColor: 'rgba(0,0,0,0.3)',
-            borderWidth: 1,
-            columns: [{
-                title: {
-                    text: 'Actividades'
-                },
-                labels: {
-                    format: '{point.name}'
-                }
-            }, {
-                title: {
-                    text: 'Responsable'
-                },
-                labels: {
-                    format: '{point.assignee}'
-                }
-            }, {
-                title: {
-                    text: 'Dias'
-                },
-                labels: {
-                    formatter: function () {
-                        var point = this.point,
-                            days = (1000 * 60 * 60 * 24),
-                            number = (point.x2 - point.x) / days;
-                        return Math.round(number * 100) / 100;
-                    }
-                }
-            }, {
-                labels: {
-                    format: '{point.start:%e. %b}'
-                },
-                title: {
-                    text: 'Fecha Inicio'
-                }
-            }, {
-                title: {
-                    text: 'Fecha Fin'
-                },
-                offset: 30,
-                labels: {
-                    format: '{point.end:%e. %b}'
-                }
-            }]
-        }
-    },
-
-    tooltip: {
-        xDateFormat: '%e %b %Y, %H:%M'
-    },
-
-    series: [{
-        name: 'Project 1',
-        data: [{
-            start: Date.UTC(2017, 10, 18, 8),
-            end: Date.UTC(2017, 10, 25, 16),
-            name: 'Start prototype',
-            assignee: 'Richards',
-            y: 0
-        }, {
-            start: Date.UTC(2017, 10, 20, 8),
-            end: Date.UTC(2017, 10, 24, 16),
-            name: 'Develop',
-            assignee: 'Michaels',
-            y: 1
-        }, {
-            start: Date.UTC(2017, 10, 25, 16),
-            end: Date.UTC(2017, 10, 25, 16),
-            name: 'Prototype done',
-            assignee: 'Richards',
-            y: 2
-        }, {
-            start: Date.UTC(2017, 10, 27, 8),
-            end: Date.UTC(2017, 11, 3, 16),
-            name: 'Test prototype',
-            assignee: 'Richards',
-            y: 3
-        }, {
-            start: Date.UTC(2017, 10, 23, 8),
-            end: Date.UTC(2017, 11, 15, 16),
-            name: 'Run acceptance tests',
-            assignee: 'Halliburton',
-            y: 4
-        }, {
-            start: Date.UTC(2017, 10, 23, 8),
-            end: Date.UTC(2017, 11, 15, 16),
-            name: 'Run acceptance tests',
-            assignee: 'Halliburton',
-            y: 5
-        }, {
-            start: Date.UTC(2017, 10, 23, 8),
-            end: Date.UTC(2017, 11, 15, 16),
-            name: 'Run acceptance tests',
-            assignee: 'Halliburton',
-            y: 6
-        }, {
-            start: Date.UTC(2017, 10, 23, 8),
-            end: Date.UTC(2017, 11, 15, 16),
-            name: 'Run acceptance tests',
-            assignee: 'Halliburton',
-            y: 7
-        }]
-    }],
-
-    exporting: {
-        sourceWidth: 1000
-    }
+$(document).ready(function () {
+    $("#btnActividades").click("click", function () {
+        ListarActividad();
+    });
 });
+
+function ListarActividad() {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        url: "Listar",
+        data: "{}",
+        dataType: "json",
+        success: function (Result) {
+
+            Result = Result.data;
+            var data = [];
+            $.each(Result, function (i, item) {
+
+                var serie = {
+                    start: cambiarFecha(item.fechaInicio),
+                    end: cambiarFecha(item.fechaFin),
+                    name: item.titActividad, y: parseInt(i)
+                }
+                data.push(serie);
+                console.log(data);
+                resultados(data);
+            })
+            },
+        error: function (Result) {
+            alert("Error");
+        }
+        });
+}
+
+function resultados(array) {
+    var data = array;
+    dibujarGrafico(data);
+}
+
+function prueba() {
+    //var serie = [];
+    //for (var i=0; i < 2; i++ ) {
+    var data = [{ end: 1623301200000, name: 'Nueva', y: 0, start: 1578459600000 },
+        { end: 1623301200000, name: 'Otra', y: 1, start: 1578459600000 }];
+    console.log(data);
+    return data;
+    //    serie.push(data);
+    //    return serie;
+    //    console.log(serie);
+        
+    //}
+    
+}
+
+function dibujarGrafico(serie) {
+    Highcharts.setOptions({
+        lang: {
+            months: [
+                'Enero', 'Febrero', 'Marzo', 'Abril',
+                'Mayo', 'Junio', 'Julio', 'Agosto',
+                'Setiembre', 'Octubre', 'Noviembre', 'Diciembre'
+            ],
+            weekdays: [
+                'Domingo', 'Lunes', 'Martes', 'Miercoles',
+                'Juevez', 'Viernes', 'Sabado'
+            ]
+        }
+    });
+
+    Highcharts.ganttChart('container', {
+
+        title: {
+            text: 'DIAGRAMA DE GANTT'
+        },
+
+        xAxis: {
+            tickPixelInterval: 70
+        },
+
+        yAxis: {
+            type: 'category',
+            grid: {
+                enabled: true,
+                borderColor: 'rgba(0,0,0,0.7)',
+                borderWidth: 1,
+                columns: [{
+                    title: {
+                        text: 'Actividad'
+                    },
+                    labels: {
+                        format: '{point.name}'
+                    }
+                }, {
+                    title: {
+                        text: 'Dias'
+                    },
+                    labels: {
+                        formatter: function () {
+                            var point = this.point,
+                                days = (1000 * 60 * 60 * 24),
+                                number = (point.x2 - point.x) / days;
+                            return Math.round(number * 100) / 100;
+                        }
+                    }
+                }, {
+                    labels: {
+                        format: '{point.start:%e. %b}'
+                    },
+                    title: {
+                        text: 'Fecha Inicio'
+                    }
+                }, {
+                    title: {
+                        text: 'Fecha Fin'
+                    },
+                    offset: 30,
+                    labels: {
+                        format: '{point.end:%e. %b}'
+                    }
+                }]
+            }
+        },
+
+        tooltip: {
+            xDateFormat: '%e %b %Y, %H:%M'
+        },
+
+        series: [{
+            name: 'Proyecto 1',
+            data: serie
+        }],
+
+        exporting: {
+            sourceWidth: 1000
+        }
+    });
+}
