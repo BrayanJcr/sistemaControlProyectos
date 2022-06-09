98e68393cafb49f48d1ba912aa9e364f28d25454
pc <75484126@continental.edu.pe>
2022-06-08 23:17:05 -0500

    asdsa

---
+++ sistemaControlProyectos/Models/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sistemaControlProyectos.Models
+{
+    public class Area
+    {
+        public int id { get; set; }
+        public string puesto { get; set; }
+        public string nombre { get; set; }
+        public Area[] hijos { get; set; }
+
+    }
+}
